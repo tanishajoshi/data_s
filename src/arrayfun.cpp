ce42ac7e6553b4f62e14a2bd594c5ba9b5fada0e
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2021-01-19 11:28:50 -0800

    using arrays in C++

--- src/arrayfun.cpp
+++ src/arrayfun.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
+
+// Give C++ code fragment for reversing an array
+void revrseArray(int arr[], int start, int end) {
+		while (start <end) {
+				int tmp = arr[start];
+				arr[start] = arr[end];
+				start ++;
+				end--;
+		}
+}
+
+void printArr(int arr[], int size) {
+		for (int i = 0; i <size; i++) 
+			cout <<arr[i]<<" ";
+			cout <<endl;
+
+}
+
 int main() {
-	// Give C++ code fragment for reducing an array
-
-	int arr_length = 15
-	for (int i = 0; i < (arr_length / 2); i++) {
-			int tmpArray= array[i];
-			array[i] = array[(arr_length - 1) - i];
-			array[(arr_length - 1) - i] = tmpArray;
-	}
-
+	int arr[] = [2,3,4,5,6,7]
+	int i = sizeof(arr)/sizeof(arr[0]);
+	printArr(arr,n);
+	revrseArray(arr,0,n-1);
+	cout << "reversed array: " << endl;
+	printArr(arr,n);
 	//Give a C++ Code fragment for randomly permuting an array. 
 
 	char ch_Array[13];
