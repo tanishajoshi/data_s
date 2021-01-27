ef246a5641032722345d7b88620cc45391e09383
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2021-01-26 22:34:49 -0800

    print the ascii val of a char in C++

---
+++ src/printascii.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//print ascii value of a char in C++
+int main()
+{
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
