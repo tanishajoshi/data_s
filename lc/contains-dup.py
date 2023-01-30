b090e93cc08072913cb1892b8b436acda82d355f
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2023-01-30 13:06:47 -0500

    count 1 bits

--- lc/contains-dup.py
+++ lc/contains-dup.py
@@ -1,10 +1,13 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate(self, nums: list[int]) -> bool:
         for i in nums:
             if (nums.count(i)>1):
                 print(nums[i])
                 return("true")
         return("false")
 
+inputs = [1,2,3,4,5,6,7,7]
+ob1 = Solution()
+print(ob1.containsDuplicate(inputs))
 
 
--- lc/count1bits.py
+++ lc/count1bits.py
@@ -4,3 +4,7 @@ class Solution:
         print(a)
         c = a.count('1')
         return c
+
+n = 7
+ob1 = Solution()
+print(ob1.hammingWeight(n))
